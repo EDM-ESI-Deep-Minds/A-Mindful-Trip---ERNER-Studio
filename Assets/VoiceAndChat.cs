adb58786118297adbbb8f0eaa4a5ee4c8bdd708a
AzzouzaMohamed <nm_azzouza@esi.dz>
2025-03-13 20:37:51 +0100

    voice dane
    il manque qulque problem don le voive a regler

---
+++ Assets/VoiceAndChat.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Unity.Multiplayer.Widgets;
+public class VoiceAndChat : MonoBehaviour
+{
+    // private SelectInputAudioDevice inputAudioDevice;
+   /* private void Start()
+    {
+        var devices = m_ChatService.GetAvailableInputDevices().ToList();
+
+        m_Dropdown.ClearOptions();
+        m_Dropdown.AddOptions(devices.Select(device => device.DeviceName).ToList());
+    }
+    public void MutePlayer()
+    {
+        var devices = m_ChatService.GetAvailableInputDevices().ToList();
+
+        // Recherche du périphérique "No Device"
+        var noDevice = devices.FirstOrDefault(device => device.DeviceName == "No Device");
+
+        if (noDevice != null)
+        {
+            // Sélectionne et active "No Device"
+            m_Dropdown.value = devices.IndexOf(noDevice);
+            noDevice.SetActiveDeviceAsync();
+        }*/
+    }
+
+
