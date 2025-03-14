c5e7c0fb1dc41c966b26700f3db4b3ce9cff62b1
AzzouzaMohamed <nm_azzouza@esi.dz>
2025-03-14 14:17:00 +0100

    le problem du public est regler don le voyce

--- Assets/VoiceAndChat.cs
+++ Assets/VoiceAndChat.cs
@@ -1,28 +1,14 @@
 using UnityEngine;
-using Unity.Multiplayer.Widgets;
+using UnityEngine.UI;
+
+
 public class VoiceAndChat : MonoBehaviour
 {
-    // private SelectInputAudioDevice inputAudioDevice;
-   /* private void Start()
-    {
-        var devices = m_ChatService.GetAvailableInputDevices().ToList();
-
-        m_Dropdown.ClearOptions();
-        m_Dropdown.AddOptions(devices.Select(device => device.DeviceName).ToList());
-    }
+    [SerializeField]
+    public Button Mute;
     public void MutePlayer()
     {
-        var devices = m_ChatService.GetAvailableInputDevices().ToList();
-
-        // Recherche du périphérique "No Device"
-        var noDevice = devices.FirstOrDefault(device => device.DeviceName == "No Device");
-
-        if (noDevice != null)
-        {
-            // Sélectionne et active "No Device"
-            m_Dropdown.value = devices.IndexOf(noDevice);
-            noDevice.SetActiveDeviceAsync();
-        }*/
+        Mute.onClick.Invoke();
     }
-
-
+    
+}
