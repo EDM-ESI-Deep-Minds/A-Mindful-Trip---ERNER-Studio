a1aaf216d98d7af27a8d13fc6a3f797a8b93071a
Rayan Derradji <nr_derradji@esi.dz>
2025-03-15 17:25:10 +0100

    Initial UI Setup

---
+++ Assets/Scripts/Inventory & Shop/ItemSO.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Objects/ItemSO")]
+public class ItemSO : ScriptableObject
+{
+    public string itemName;
+    [TextArea] public string itemDescription;
+    [TextArea] public string itemEffect;
+    public Sprite itemIcon;
+    public bool isCoin;
+
+    [Header("Stats")]
+    public int currentHealth;
+    public int maxHealth;
+    public int itemBaseCost;
+
+    [Header("One-Time")]
+    public bool oneTimeUse;
+}
