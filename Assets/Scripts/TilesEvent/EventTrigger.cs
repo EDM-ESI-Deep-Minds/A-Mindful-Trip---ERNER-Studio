cf2718e720b8ae9fef17fd71cb5f1d9c9e5b7844
AzzouzaMohamed <nm_azzouza@esi.dz>
2025-03-24 13:00:02 +0100

    Add EventTrigger class with SelectEventToTrigger method for handling tile events

---
+++ Assets/Scripts/TilesEvent/EventTrigger.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public static class EventTrigger
+{
+    public static void SelectEventToTrigger(string TileType)
+    {
+        switch (TileType)
+        {
+            case "Rest":
+                
+                break;
+
+            case "Bonus":
+                
+                break;
+
+            case "Curse":
+               
+                break;
+
+            case "Question":
+                
+                break;
+
+            default:
+              
+                break;
+        }
+    }
+}
