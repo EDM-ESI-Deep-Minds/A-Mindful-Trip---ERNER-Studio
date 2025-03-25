363820d2cb7e8402577242310fc24cfcf902d1f6
AzzouzaMohamed <nm_azzouza@esi.dz>
2025-03-26 00:01:55 +0100

    sync cliont

--- Assets/RolesManager.cs
+++ Assets/RolesManager.cs
@@ -7,10 +7,20 @@ using System.Threading;
 public class RolesManager : NetworkBehaviour
 {
     public static bool IsMyTurn;
+     public SwitcheCam switcheCam;
+
+
     private void Start()
     {
         IsMyTurn = SwitcheCam.CurrentPlayer.IsOwner;
-
+        switcheCam = FindFirstObjectByType<SwitcheCam>();
     }
-   
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switcheCam.ChaingeTargetServerRpc();
+        }
+    }
 }
