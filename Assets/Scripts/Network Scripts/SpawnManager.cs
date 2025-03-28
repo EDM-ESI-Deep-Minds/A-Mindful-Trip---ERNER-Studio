4be3acd311d4675912b1349315590c282cb839b0
Tarek Ait Ahmed <159696248+tarek-ait@users.noreply.github.com>
2025-03-28 17:10:31 +0100

    update the city map, save the starting positions

--- Assets/Scripts/Network Scripts/SpawnManager.cs
+++ Assets/Scripts/Network Scripts/SpawnManager.cs
@@ -15,9 +15,23 @@ public class spawn_mang : NetworkBehaviour
      {-0.56f, 0.08f},
      {1.52f, 5.04f},
      {2.48f, -3.28f},
-   
-    
  };
+    float[,] city_map_positions = new float[4, 2]
+   {
+        { -4.32f, -11.36f },
+        { -4.32f, -7.52f },
+        { -4.32f, -4.00f },
+        { -4.32f, 0.80f },
+   };
+
+    float[,] desert_map_positions = new float[4, 2]
+   {
+    { -0.56f, -5.04f },
+    { -0.56f, -3.12f },
+    { -0.56f, -1.68f },
+    { -0.56f,  0.08f }
+   };
+
     int index_position=-1;
     public static GameObject[] AllPlayer = new GameObject[4];
     public static int IndexTabAllPlayer = 0;
@@ -60,7 +74,7 @@ public class spawn_mang : NetworkBehaviour
     private Vector2 GetSpawnPosition()
     {
         index_position++;
-        return new Vector2(les_position[index_position,0], les_position[index_position, 1]);
+        return new Vector2(city_map_positions[index_position,0], city_map_positions[index_position, 1]);
       
     }
 
@@ -83,7 +97,7 @@ public class spawn_mang : NetworkBehaviour
         // NetworkManager.SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         RoomUIManager roomUIManager = FindObjectOfType<RoomUIManager>();
         SelectedCharacters.Instance.SetSelectedCharacters(roomUIManager.GetSelectedCharacters());
-        NetworkManager.SceneManager.LoadScene("Desert", LoadSceneMode.Single);
+        NetworkManager.SceneManager.LoadScene("City", LoadSceneMode.Single);
 
     }
     [ClientRpc]
