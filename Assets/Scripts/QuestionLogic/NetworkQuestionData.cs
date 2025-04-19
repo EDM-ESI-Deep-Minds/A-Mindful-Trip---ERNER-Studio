09794bed94e290aadf3c802b5d6398cc60d9d3fd
Rayan Derradji <nr_derradji@esi.dz>
2025-04-19 10:55:05 +0100

    initial question event is fully functional

---
+++ Assets/Scripts/QuestionLogic/NetworkQuestionData.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
+using Unity.Netcode;
+
+public struct NetworkQuestionData : INetworkSerializable
+{
+    public FixedString512Bytes questionText;
+    public FixedString128Bytes correctAnswer;
+    public FixedString128Bytes answer1;
+    public FixedString128Bytes answer2;
+    public FixedString128Bytes answer3;
+    public FixedString128Bytes answer4;
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref questionText);
+        serializer.SerializeValue(ref correctAnswer);
+        serializer.SerializeValue(ref answer1);
+        serializer.SerializeValue(ref answer2);
+        serializer.SerializeValue(ref answer3);
+        serializer.SerializeValue(ref answer4);
+    }
+
+    public string[] GetShuffledAnswers()
+    {
+        List<string> answers = new();
+        if (!string.IsNullOrWhiteSpace(answer1.ToString())) answers.Add(answer1.ToString());
+        if (!string.IsNullOrWhiteSpace(answer2.ToString())) answers.Add(answer2.ToString());
+        if (!string.IsNullOrWhiteSpace(answer3.ToString())) answers.Add(answer3.ToString());
+        if (!string.IsNullOrWhiteSpace(answer4.ToString())) answers.Add(answer4.ToString());
+        return answers.OrderBy(_ => UnityEngine.Random.value).ToArray();
+    }
+}
--- Assets/Scripts/QuestionLogic/QuestionManager.cs
+++ Assets/Scripts/QuestionLogic/QuestionManager.cs
@@ -2,6 +2,9 @@ using Unity.Netcode;
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 
 public class QuestionManager : NetworkBehaviour
 {
@@ -9,11 +12,14 @@ public class QuestionManager : NetworkBehaviour
 
     [SerializeField] private GameObject questionUIPrefab;
     private GameObject spawnedUI;
+    [SerializeField] private GameObject dice1;
+    [SerializeField] private GameObject dice2;
 
     private string correctAnswer;
     private bool hasAnswered = false;
     private float timerLeft;
     private int currentCategory;
+    private ulong currentPlayerId;
 
 
     private void Awake()
@@ -31,16 +37,16 @@ public class QuestionManager : NetworkBehaviour
         EventTrigger.OnQuestionTile -= OnQuestionTileTriggered;
     }
 
-    private void OnQuestionTileTriggered()
-    {
-        if (!IsServer) return;
+    // private void OnQuestionTileTriggered()
+    // {
+    //     if (!IsServer) return;
 
-        int category = QuestionLoader.Instance.GetRandomCategory();
-        string difficulty = DifficultySelector.GetQuestionDifficulty(ProfileManager.SelectedProfile.Elo);
-        string questionType = QuestionLoader.Instance.GetRandomQuestionType();
+    //     int category = QuestionLoader.Instance.GetRandomCategory();
+    //     string difficulty = DifficultySelector.GetQuestionDifficulty(ProfileManager.SelectedProfile.Elo);
+    //     string questionType = QuestionLoader.Instance.GetRandomQuestionType();
 
-        SpawnQuestionClientRpc(category, difficulty, questionType, 60f);
-    }
+    //     SpawnQuestionClientRpc(category, difficulty, questionType, 60f);
+    // }
 
     // public void LocalPlayerLoadQuestion()
     // {
@@ -50,16 +56,48 @@ public class QuestionManager : NetworkBehaviour
 
     //     SpawnQuestionClientRpc(category, difficulty, questionType, 30f);
     // }
-
-
-    [ClientRpc]
-    private void SpawnQuestionClientRpc(int category, string difficulty, string questionType, float timer)
+    private void OnQuestionTileTriggered()
     {
-        currentCategory = category; // Storing current category to calculate elo later
+        // if (!IsOwner) return; // Only the player who landed on the tile proceeds
+
+        int category = QuestionLoader.Instance.GetRandomCategory();
+        string difficulty = DifficultySelector.GetQuestionDifficulty(ProfileManager.SelectedProfile.Elo);
+        string questionType = QuestionLoader.Instance.GetRandomQuestionType();
 
         Question question = QuestionLoader.Instance.LoadQuestion(category, difficulty, questionType);
-        string[] answers = QuestionLoader.Instance.GetAnswers(question);
-        correctAnswer = question.correct_answer;
+        currentCategory = category;
+
+        string[] allAnswers = QuestionLoader.Instance.GetAnswers(question);
+
+        NetworkQuestionData questionData = new()
+        {
+            questionText = question.question,
+            correctAnswer = question.correct_answer,
+            answer1 = allAnswers.Length > 0 ? allAnswers[0] : "",
+            answer2 = allAnswers.Length > 1 ? allAnswers[1] : "",
+            answer3 = allAnswers.Length > 2 ? allAnswers[2] : "",
+            answer4 = allAnswers.Length > 3 ? allAnswers[3] : ""
+        };
+
+        SendQuestionToServerRpc(questionData, category, 60f);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SendQuestionToServerRpc(NetworkQuestionData questionData, int category, float timer, ServerRpcParams rpcParams = default)
+    {
+        currentPlayerId = rpcParams.Receive.SenderClientId;
+        BroadcastQuestionClientRpc(questionData, category, timer, currentPlayerId);
+    }
+
+    [ClientRpc]
+    private void BroadcastQuestionClientRpc(NetworkQuestionData questionData, int category, float timer, ulong answeringPlayerId)
+    {
+        currentCategory = category;
+        correctAnswer = questionData.correctAnswer.ToString();
+        hasAnswered = false;
+        timerLeft = timer;
+
+        bool isMyTurn = NetworkManager.Singleton.LocalClientId == answeringPlayerId;
 
         HideGameplayUI(true);
 
@@ -67,16 +105,42 @@ public class QuestionManager : NetworkBehaviour
         var ui = spawnedUI.GetComponent<QuestionUI>();
 
         ui.InitializeUI();
-
-        timerLeft = timer;
-        hasAnswered = false;
-
-        StartCoroutine(HandleQuestionSequence(ui, question.question, answers, timer, RolesManager.IsMyTurn));
+        // StartCoroutine(HandleQuestionSequence(ui, questionData.questionText.ToString(), questionData.GetShuffledAnswers(), timer, isMyTurn));
+        StartCoroutine(HandleQuestionSequence(
+            ui,
+            WebUtility.HtmlDecode(questionData.questionText.ToString()),
+            questionData.GetShuffledAnswers().Select(WebUtility.HtmlDecode).ToArray(),
+            timer,
+            isMyTurn
+        ));
     }
+
+
+    // [ClientRpc]
+    // private void SpawnQuestionClientRpc(int category, string difficulty, string questionType, float timer)
+    // {
+    //     currentCategory = category; // Storing current category to calculate elo later
+
+    //     Question question = QuestionLoader.Instance.LoadQuestion(category, difficulty, questionType);
+    //     string[] answers = QuestionLoader.Instance.GetAnswers(question);
+    //     correctAnswer = question.correct_answer;
+
+    //     HideGameplayUI(true);
+
+    //     spawnedUI = Instantiate(questionUIPrefab, GameObject.Find("Canvas").transform);
+    //     var ui = spawnedUI.GetComponent<QuestionUI>();
+
+    //     ui.InitializeUI();
+
+    //     timerLeft = timer;
+    //     hasAnswered = false;
+
+    //     StartCoroutine(HandleQuestionSequence(ui, question.question, answers, timer, RolesManager.IsMyTurn));
+    // }
 
     private IEnumerator HandleQuestionSequence(QuestionUI ui, string questionText, string[] answers, float timer, bool isMyTurn)
     {
-        ui.ShowIntroDialogue("Cellica: Ready for a challenge?");
+        ui.ShowIntroDialogue("Ready for a challenge?");
 
         yield return new WaitForSeconds(3f); // Intro duration
 
@@ -138,18 +202,20 @@ public class QuestionManager : NetworkBehaviour
                     heartUI.removeHeart();
                 }
             }
-            
+
             ProfileManager.PlayerProfile profile = ProfileManager.SelectedProfile;
             EloCalculator.UpdateCategoryElo(profile, currentCategory.ToString(), isCorrect, 1);
 
             Invoke(nameof(SwitchTurn), 3f);
         }
+        Invoke(nameof(CleanupQuestionUIClientRpc), 3f);
     }
 
     private void SwitchTurn()
     {
-        Destroy(spawnedUI);
-        HideGameplayUI(false);
+        // Destroy(spawnedUI);
+        // HideGameplayUI(false);
+        CleanupQuestionUIClientRpc();
         RolesManager.SwitchRole();
     }
 
@@ -162,5 +228,22 @@ public class QuestionManager : NetworkBehaviour
             var obj = GameObject.Find(name);
             if (obj != null) obj.SetActive(!hide);
         }
+
+        // Special treatment for Dice1 and Dice2
+        if (dice1 != null) dice1.SetActive(!hide);
+        if (dice2 != null) dice2.SetActive(!hide);
     }
+
+    [ClientRpc]
+    private void CleanupQuestionUIClientRpc()
+    {
+        if (spawnedUI != null)
+        {
+            Destroy(spawnedUI);
+            spawnedUI = null;
+        }
+
+        HideGameplayUI(false);
+    }
+
 }
