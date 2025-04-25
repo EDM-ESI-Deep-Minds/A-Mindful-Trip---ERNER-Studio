4f70cc5a1540e8d9f0cfa86732146fa567407a88
Rayan Derradji <nr_derradji@esi.dz>
2025-04-25 20:04:36 +0100

    added missing tags

--- Assets/Scripts/WinEvents/ToNextMap.cs
+++ Assets/Scripts/WinEvents/ToNextMap.cs
@@ -1,7 +1,7 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class NewEmptyCSharpScript : NetworkBehaviour
+public class ToNextMap : NetworkBehaviour
 {
     private static bool DesertDone = false;
 
