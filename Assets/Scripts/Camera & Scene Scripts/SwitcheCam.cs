7a2192c914f232b89b3b382beea93c72eee276ee
Tarek Ait Ahmed <159696248+tarek-ait@users.noreply.github.com>
2025-04-25 19:23:40 +0200

    unsubscribing from the events on the scripts

--- Assets/Scripts/Camera & Scene Scripts/SwitcheCam.cs
+++ Assets/Scripts/Camera & Scene Scripts/SwitcheCam.cs
@@ -20,8 +20,17 @@ public class SwitcheCam : NetworkBehaviour
         RolesManager.CameraSwitchTarget += ChaingeTargetServerRpc;
     }
 
+    private void OnDisable()
+    {
+        
+        RolesManager.CameraSwitchTarget -= ChaingeTargetServerRpc;
+    }
 
-
+    private void OnDestroy()
+    {
+        
+        RolesManager.CameraSwitchTarget -= ChaingeTargetServerRpc;
+    }
 
     void Update()
     {
