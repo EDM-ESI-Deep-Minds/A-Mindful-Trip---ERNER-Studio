1c812785dc487a625aac987b7113435d154f00ed
Hadim anes <92207623+Anes-Hadim@users.noreply.github.com>
2025-04-26 14:03:35 +0100

    removed errored name space

--- Assets/Scripts/Inventory & Shop/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory & Shop/Inventory/InventoryManager.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 
 public class InventoryManager : MonoBehaviour
 {
