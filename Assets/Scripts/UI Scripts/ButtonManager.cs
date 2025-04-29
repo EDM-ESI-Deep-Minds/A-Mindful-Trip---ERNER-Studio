8896da675caa44152d2e8ffdbc274efc394a9495
Hadim anes <92207623+Anes-Hadim@users.noreply.github.com>
2025-04-29 10:19:28 +0100

    fixed incative return buttons when reply or failed join

--- Assets/Scripts/UI Scripts/ButtonManager.cs
+++ Assets/Scripts/UI Scripts/ButtonManager.cs
@@ -8,14 +8,26 @@ public class ButtonManager : MonoBehaviour
     public Toggle radioButton1;
     public Toggle radioButton2;
 
-    public void Create()
+    public void CreateDisable()
     {
         targetButton.interactable = false;
         radioButton1.interactable = false;
         radioButton2.interactable = false;
     }
 
-    public void Join()
+    public void CreateEnable()
+    {
+        targetButton.interactable = false;
+        radioButton1.interactable = false;
+        radioButton2.interactable = false;
+    }
+
+    public void JoinDisable()
+    {
+        targetButton.interactable = false;
+    }
+
+    public void JoinEnable()
     {
         targetButton.interactable = false;
     }
