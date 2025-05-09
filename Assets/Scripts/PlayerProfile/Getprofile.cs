6b6f6ab3967b4d6c91a65a201701dbdecc986c11
Hadim anes <92207623+Anes-Hadim@users.noreply.github.com>
2025-05-09 15:05:33 +0100

    implemented elo ranks

--- Assets/Scripts/PlayerProfile/Getprofile.cs
+++ Assets/Scripts/PlayerProfile/Getprofile.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GetProfile : MonoBehaviour
 {
     public TMP_Text profileText;
+    public Image rank;
+    public Sprite[] ranks;
+
+    private int elo;
 
     void Start()
     {
@@ -11,17 +16,34 @@ public class GetProfile : MonoBehaviour
         {
             string playerName = ProfileManager.SelectedProfile.playerName;
             int playerElo = ProfileManager.SelectedProfile.Elo;
+            elo = playerElo;
             profileText.text = $"Player: {playerName}\nElo: {playerElo}";
         }
         else if (PlayerPrefs.HasKey("PlayerName"))
         {
             string playerName = PlayerPrefs.GetString("PlayerName");
             int playerElo = PlayerPrefs.GetInt("Elo");
+            elo = playerElo;
             profileText.text = $"Player: {playerName}\nElo: {playerElo}";
         }
         else
         {
             profileText.text = "No profile selected!";
+            elo = 0;
+        }
+
+        if (elo <1000)
+        {
+            rank.sprite = ranks[0];
+        } else if (elo <1800)
+        {
+            rank.sprite = ranks[1];
+        } else if (elo <2500)
+        {
+            rank.sprite = ranks[2];
+        } else
+        {
+            rank.sprite = ranks[3];
         }
     }
 }
