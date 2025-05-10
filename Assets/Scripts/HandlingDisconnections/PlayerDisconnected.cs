a39dcaff6c6b74b655ca2c250f023ba9f6c265e9
AzzouzaMohamed <nm_azzouza@esi.dz>
2025-05-10 18:39:02 +0100

    dane

--- Assets/Scripts/HandlingDisconnections/PlayerDisconnected.cs
+++ Assets/Scripts/HandlingDisconnections/PlayerDisconnected.cs
@@ -2,18 +2,15 @@ using System;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class PlayerDisconnected : NetworkBehaviour
 {
     [SerializeField] private Button leaveButtonObject;
 
     private void OnEnable()
     {
-        // S'abonner à l'événement de déconnexion du client local
+        // Abonnement à l'événement de déconnexion du client
         NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
-
-        // S'abonner également à l'événement de transport de déconnexion
-        // Ce qui est déclenché quand le client local se déconnecte
-       // NetworkManager.Singleton.NetworkConfig.NetworkTransport.OnTransportEvent += OnTransportEvent;
     }
 
     private void OnDisable()
@@ -21,41 +18,17 @@ public class PlayerDisconnected : NetworkBehaviour
         if (NetworkManager.Singleton != null)
         {
             NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnected;
-
-           /* if (NetworkManager.Singleton.NetworkConfig.NetworkTransport != null)
-            {
-                NetworkManager.Singleton.NetworkConfig.NetworkTransport.OnTransportEvent -= OnTransportEvent;
-            }*/
         }
     }
 
     private void OnClientDisconnected(ulong clientId)
     {
-        Debug.LogWarning("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
-
-       // NetworkManager.Singleton.Shutdown();
-         leaveButtonObject.onClick.Invoke();
+        // Appel du bouton de retour, réinitialisation des variables, et retour au menu principal
+        leaveButtonObject.onClick.Invoke();
         spawn_mang.IndexTabAllPlayer = 0;
         spawn_mang.SpawanDone = false;
         spawn_mang.AllPlayer = new GameObject[4];
         ToNextMap.DesertDone = false;
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
-       
     }
-
-    /*private void OnTransportEvent(NetworkEvent eventType, ulong clientId, ArraySegment<byte> payload, float receiveTime)
-    {
-        // Détecte quand le client local perd la connexion
-        if (eventType == NetworkEvent.Disconnect)
-        {
-            Debug.Log("Transport déconnecté (involontaire ou volontaire)");
-            ReturnToMainMenu();
-        }
-    }*/
-
-   /* private void ReturnToMainMenu()
-    {
-        // Fonction commune pour revenir au menu principal
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
-    }*/
 }
